f24a5f29bf8f218684d019e2d978ab2fc378a9dd
Andrew Livigni <152040968+DrewBagelz@users.noreply.github.com>
2024-03-16 21:58:47 -0400

    Create function to play five rounds and keep score

--- javascript.js
+++ javascript.js
@@ -1,46 +1,54 @@
 const choice = ["rock", "paper", "scissors"];
 
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
 	return choice[Math.floor(Math.random() * choice.length)];
 }
-
-// function getPlayerChoice() {
-//   let input = prompt("Choose rock, paper, or scissors:");
-//   input = input.toLowerCase();
-// }
 
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection === "rock" && computerSelection === "rock") {
 		return "Both chose rock. This round ends in a tie.";
 	} else if (playerSelection === "rock" && computerSelection === "paper") {
+		computerScore++;
 		return "Paper beats rock. Computer wins this round.";
 	} else if (playerSelection === "rock" && computerSelection === "scissors") {
+		playerScore++;
 		return "Rock beats scissors. You win this round.";
 	} else if (playerSelection === "paper" && computerSelection === "rock") {
+		playerScore++;
 		return "Paper beats rock. You win this round.";
 	} else if (playerSelection === "paper" && computerSelection === "paper") {
 		return "Both chose paper. This round ends in a tie.";
 	} else if (playerSelection === "paper" && computerSelection === "scissors") {
+		computerScore++;
 		return "Scissors beats paper. Computer wins this round.";
 	} else if (playerSelection === "scissors" && computerSelection === "rock") {
+		computerScore++;
 		return "Rock beats scissors. Computer wins this round.";
 	} else if (playerSelection === "scissors" && computerSelection === "paper") {
+		playerScore++;
 		return "Scissors beats paper. You win this round.";
 	} else if (playerSelection === "scissors" && computerSelection === "scissors") {
 		return "Both chose scissors. This round ends in a tie.";
 	}
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function playGame() {
+	for (let i = 0; i < 5; i++) {
+		const playerSelection = prompt("Choose rock, paper, or scissors:").toLowerCase();
+		const computerSelection = getComputerChoice();
+		console.log("1 ", playRound(playerSelection, computerSelection));
+	}
 
-//Write a NEW function called playGame(). Use the previous function inside of this one to play a five round game that keeps score and reports a winner or loser at the end.
+	if (playerScore > computerScore) {
+		return playerScore + "-" + computerScore + ", You win!";
+	} else if (computerScore > playerScore) {
+		return computerScore + "-" + playerScore + ", Computer wins.";
+	} else {
+		return playerScore + "-" + computerScore + ", This game ends in a tie.";
+	}
+}
 
-//At this point you should be using console.log() to display the results of each round and the winner at the end.
-
-//Use prompt() to get input from the user.
-
-//function playGame() {
-//code here
-//}
+console.log(playGame());
